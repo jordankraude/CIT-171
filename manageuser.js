658a5180e9c0fd566c4f74a251254a1415c07844
jordankraude <jordankraudeTP@gmail.com>
2022-01-27 15:31:42 -0700

    -changes to code to allow 2 factor

--- manageuser.js
+++ manageuser.js
@@ -11,11 +11,11 @@ function setusername(){
 
 function setuserpassword(){
     password = $("#password").val();
-    var valid=passwordRegEx.exec(password);
-    if (!valid){
-        alert('Must be 6 digits, upper, lower, number, and symbol');
+    //var valid=passwordRegEx.exec(password);
+    //if (!valid){
+    //    alert('Must be 6 digits, upper, lower, number, and symbol');
     }
-}
+
 
 function setverifypassword(){
     verifypassword = $("#verifypassword").val();
@@ -47,17 +47,30 @@ function checkexpiredtoken(token){
 }
 
 function userlogin(){
+    setusername();
+    $.ajax({
+        type: 'POST',
+        url: 'https://dev.stedi.me/twofactorlogin/'+userName,
+        data: JSON.stringify({userName, password}),
+        //success: function(data) {
+        //    window.location.href = "/timer.html#"+data;//add the token to the url
+        //},
+        contentType: "application/text",
+        dataType: 'text'
+    });
+
+}
+
+function twofactorlogin(){
     setuserpassword();
     setusername();
     $.ajax({
         type: 'POST',
-        url: '/login',
-        data: JSON.stringify({userName, password}),
+        url: 'https://dev.stedi.me/twofactorlogin' ,
+        data: JSON.stringify({phoneNumber: userName, oneTimePassword: password}),
         success: function(data) {
             window.location.href = "/timer.html#"+data;//add the token to the url
-        },
-        contentType: "application/text",
-        dataType: 'text'
+        }
     });
 
 }
